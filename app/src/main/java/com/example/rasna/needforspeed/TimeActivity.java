b483f0124746d9cae490d8af849e4669da5c250e
Rasna Rahman <rahman.rahman@gmail.com>
2017-11-19 11:13:34 -0500

    Android Project_NeedForSpeed

---
+++ app/src/main/java/com/example/rasna/needforspeed/TimeActivity.java
@@ -0,0 +1,20 @@
+package com.example.rasna.needforspeed;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class TimeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_time );
+    }
+
+    public void clickedOk(View view) {
+    }
+
+    public void clickedCancel(View view) {
+    }
+}
---
+++ app/src/main/java/com/example/rasna/needforspeed/gas_logList.java
@@ -0,0 +1,13 @@
+package com.example.rasna.needforspeed;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class gas_logList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_gas_log );
+    }
+}
