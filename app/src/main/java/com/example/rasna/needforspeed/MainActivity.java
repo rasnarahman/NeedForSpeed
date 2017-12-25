c6981027c31c48c1591fe3e7a8b5a93dc51d5817
Rasna Rahman <rahman.rahman@gmail.com>
2017-12-25 13:00:22 -0500

    Pass VIN to the gas log page

--- app/src/main/java/com/example/rasna/needforspeed/MainActivity.java
+++ app/src/main/java/com/example/rasna/needforspeed/MainActivity.java
@@ -17,6 +17,8 @@ import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends Activity {
     DatabaseHelper dbHelper;
@@ -39,9 +41,10 @@ public class MainActivity extends Activity {
         List<String> vechileMakeModelYearStringList = new ArrayList<String>();
         List<Vehicle> vehicleList = dbHelper.getAllVehicles();
         spinnerDropDown = (Spinner) findViewById(R.id.spinnerVehicles);
+        vechileMakeModelYearStringList.add("----- Vehicles -----");
 
         for (Vehicle vehicle: vehicleList) {
-            vechileMakeModelYearStringList.add(vehicle.Make + " - " + vehicle.Model + " - " + vehicle.Year);
+            vechileMakeModelYearStringList.add(vehicle.Make + " " + vehicle.Model + " " + vehicle.Year + " (" + vehicle.Vin + ")");
         }
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, vechileMakeModelYearStringList);
@@ -52,9 +55,14 @@ public class MainActivity extends Activity {
         {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
-                Toast.makeText(parent.getContext(),
-                        "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
-                        Toast.LENGTH_SHORT).show();
+                if(pos != 0 ) {
+                    String selectedVehicle = parent.getItemAtPosition(pos).toString();
+                    Toast.makeText(parent.getContext(), "Selected Vehicle : " + selectedVehicle, Toast.LENGTH_SHORT).show();
+
+                    Intent intent = new Intent(MainActivity.this, gas_logList.class);
+                    intent.putExtra("CURRENT_VEHICLE_VIN", extractVinFromVehicleInfo(selectedVehicle));
+                    startActivity( intent );
+                }
             }
 
             @Override
@@ -63,6 +71,18 @@ public class MainActivity extends Activity {
             }
         });
 
+    }
+
+    private String extractVinFromVehicleInfo(String vehicleInfo) {
+        Pattern pattern = Pattern.compile(".*([(].*[)])");
+        Matcher matcher = pattern.matcher(vehicleInfo);
+        if(matcher.matches()) {
+            String vin = matcher.group(1);
+            return vin.substring(1, vin.length() - 1);
+        }
+        else {
+            return "";
+        }
     }
 
     private void setListAdapter(ArrayAdapter<String> adapter) {
--- app/src/main/java/com/example/rasna/needforspeed/gas_logList.java
+++ app/src/main/java/com/example/rasna/needforspeed/gas_logList.java
@@ -2,6 +2,8 @@ package com.example.rasna.needforspeed;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 public class gas_logList extends Activity {
 
@@ -9,5 +11,8 @@ public class gas_logList extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_gas_log );
+        String currentVehicleVin = getIntent().getStringExtra("CURRENT_VEHICLE_VIN");
+        Log.i("GasLogActivity", "Current Vehicle VIN = " + currentVehicleVin);
+
     }
 }
